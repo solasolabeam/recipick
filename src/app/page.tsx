f971566cf14a5b083461e2b9b1554c040d8ab6ad
sol <nathf1005@naver.com>
2024-12-17 01:09:15 +0900

    add: 메인화면 와이어프레임 작성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,62 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+    <div className="mx-5">
+      <div className="mt-9 text-4xl font-bold">LOGO</div>
+      <section className="mt-3 w-90 h-10 border-none rounded">
+        <input
+          className="w-full h-full bg-inputGray rounded text-xs p-4"
+          placeholder="재료 또는 레시피 검색"
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      </section>
+      <div className="mt-5 w-90 h-[230px] border-none bg-gray-200 rounded"></div>
+      <section className="mt-16">
+        <p className="text-xs">냉장고에 뭐가 있나요?</p>
+      </section>
+      <section className="flex flex-col gap-3 mt-3 items-center">
+        <input
+          type="text"
+          placeholder="재료 입력"
+          className="w-[312px] h-10 border-none bg-inputGray rounded text-xs p-4"
+        />
+        <button className="w-[312px] h-10 border-none bg-materialGet rounded text-white text-xs">
+          불러오기
+        </button>
+        <button className="w-[312px] h-10 border-none bg-recipeFind rounded text-white text-xs">
+          레시피 찾기
+        </button>
+      </section>
+      <section className="mt-16">
+        <div>
+          <p className="text-2xl font-bold">추천 레시피</p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="mt-4">
+          <div className="w-52 h-[220px] border border-black">
+            <div className="w-full h-40 border-none bg-gray-200"></div>
+            <div className="w-full h-[60px] pl-6">
+              <p className="pt-3 text-base">된장찌개</p>
+              <p className="pt-1 text-xs text-materialAdd">
+                구수한 된장찌개 레시피
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="mt-16">
+        <div>
+          <p className="text-2xl font-bold">인기 레시피</p>
+        </div>
+        <div className="mt-4">
+          <div className="w-52 h-[220px] border border-black">
+            <div className="w-full h-40 border-none bg-gray-200"></div>
+            <div className="w-full h-[60px] pl-6">
+              <p className="pt-3 text-base">김치찌개</p>
+              <p className="pt-1 text-xs text-materialAdd">
+                매콤한 한국 전통 찌개
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
