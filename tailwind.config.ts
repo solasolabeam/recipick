b0f9061a3b92d3ca5d272166eeb0ecdbb11dd71f
sol <nathf1005@naver.com>
2024-12-31 01:05:14 +0900

    add: 카테고리 텍스트 컬러 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,11 +14,18 @@ export default {
         recipeFind: "#FFA2A2",
         recipeSearch: "#B56D40",
         freezerSearch: "#BFB2A9",
+        // 카테고리 배경
         rice: "#FDCD4F",
         soup: "#FFA86A",
         sideDish: "#C6BCA2",
         dessert: "#DCBDBD",
         best: "#818E6F",
+        // 카테고리 텍스트
+        riceText: "#A27500",
+        soupText: "#AB4700",
+        sideDishText: "#513A00",
+        dessertText: "#A06262",
+        bestText: "white",
       },
     },
   },
