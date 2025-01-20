a6c5676abb3b590d05645412e9aceb8d50f29471
sna <nathf1005@naver.com>
2025-01-20 16:10:02 +0900

    fix:  네트워크 응답 확인 코드 주석 처리

--- src/app/(components)/MainPage.tsx
+++ src/app/(components)/MainPage.tsx
@@ -31,9 +31,9 @@ const getData = async (startIndex: number, endIndex: number) => {
     `http://openapi.foodsafetykorea.go.kr/api/de77957df6d04d03a521/COOKRCP01/json/${startIndex}/${endIndex}`,
   );
 
-  if (!res.ok) {
-    throw new Error("Network response was not ok");
-  }
+  // if (!res.ok) {
+  //   throw new Error("Network response was not ok");
+  // }
 
   return res.json();
 };
