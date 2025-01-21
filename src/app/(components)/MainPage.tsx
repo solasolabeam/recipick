924bea8ecf3ed483f0bd01259197ee6d43537208
sna <nathf1005@naver.com>
2025-01-21 15:47:26 +0900

    feat: RankList 및 RankListLoading 컴포넌트를 새로 추가하고 MainPage에서 사용하도록 수정

--- src/app/(components)/MainPage.tsx
+++ src/app/(components)/MainPage.tsx
@@ -9,14 +9,10 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import { useQuery } from "@tanstack/react-query";
 import Image from "next/image";
 import "swiper/css";
-import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination } from "swiper/modules";
-import { useIsFetching, useQuery } from "@tanstack/react-query";
-import { recipeProps, searchProps } from "../type/recipe";
-import getData from "@/app/util/getData";
+import { useIsFetching } from "@tanstack/react-query";
 import AllList, { AllListLoading } from "./AllList";
+import RankList, { RankListLoading } from "./RankList";
 
 //메인 이미지
 const banner = "/assets/images/banner.png";
@@ -191,82 +187,3 @@ export default function MainPage() {
     </>
   );
 }
-
-const RankList = ({ startIndex, endIndex, queryKey }: searchProps) => {
-  const {
-    data: data,
-    error,
-    isError,
-  } = useQuery({
-    queryKey: [queryKey],
-    queryFn: () => getData(startIndex, endIndex),
-    staleTime: 300000,
-  });
-
-  if (isError) {
-    // 에러 처리
-    console.error("Error!:", error);
-    return <div>Error occurred!</div>;
-  }
-
-  return (
-    <Swiper
-      slidesPerView={2}
-      spaceBetween={100}
-      pagination={{
-        clickable: true,
-      }}
-      modules={[Pagination]}
-      className=""
-    >
-      {data?.COOKRCP01.row.map((recipe: recipeProps) => (
-        <SwiperSlide key={recipe.RCP_SEQ} className="mt-4">
-          <div className="w-52 border border-none">
-            <div className="h-40 w-full">
-              <Image
-                className="h-full w-full rounded-xl object-cover"
-                src={recipe.ATT_FILE_NO_MK}
-                alt={recipe.RCP_NM}
-                width={450}
-                height={450}
-              ></Image>
-            </div>
-            <div className="h-[146px] w-full p-5">
-              <p className="text-base font-semibold">{recipe.RCP_NM}</p>
-              <p className="text-materialAdd line-clamp-2 pt-3 text-sm">
-                {recipe.RCP_NA_TIP}
-              </p>
-            </div>
-          </div>
-        </SwiperSlide>
-      ))}
-    </Swiper>
-  );
-};
-
-const RankListLoading = () => {
-  return (
-    <Swiper
-      slidesPerView={2}
-      spaceBetween={150}
-      pagination={{
-        clickable: true,
-      }}
-      modules={[Pagination]}
-      className=""
-    >
-      {Array.from({ length: 6 }).map((_, idx) => (
-        <SwiperSlide className="" key={idx}>
-          <div className="w-52 flex-none border border-none">
-            <Skeleton height="160px" width="100%" />
-            <div className="h-[146px] w-full p-5">
-              <Skeleton width="50%" />
-              <Skeleton width="100%" />
-              <Skeleton width="100%" />
-            </div>
-          </div>
-        </SwiperSlide>
-      ))}
-    </Swiper>
-  );
-};
