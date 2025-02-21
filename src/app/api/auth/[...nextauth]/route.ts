802d34c8133b1b938a119b4b272203d4cad60d81
solar <nathf1005@naver.com>
2025-02-22 02:14:48 +0900

    refactor: NextAuth 설정을 위한 authOptions 변수 추가 및 핸들러 정의 변경

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import KakaoProvider from "next-auth/providers/kakao";
 
-export const handler = NextAuth({
+const authOptions = {
   providers: [
     KakaoProvider({
       clientId: process.env.KAKAO_CLIENT_ID || "", // 카카오 앱에서 발급 받은 클라이언트 ID
@@ -9,6 +9,8 @@ export const handler = NextAuth({
     }),
   ],
   // 추가 설정 (콜백, 데이터베이스 연결 등)
-});
+};
+
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
