97d2b949f4bbb8dc346793238c83fddd418e9cd6
solar <nathf1005@naver.com>
2025-02-26 16:02:07 +0900

    add: 유틸리티 함수 및 Radix UI Select 컴포넌트 추가, Tailwind CSS 설정 업데이트

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,42 +1,85 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        inputGray: "#F1F1F1",
-        materialGet: "#9A9A9A",
-        recipeFind: "#FFA2A2",
-        recipeSearch: "#B56D40",
-        freezerSearch: "#BFB2A9",
-        // 카테고리 배경
-        rice: "#FDCD4F",
-        soup: "#FFA86A",
-        sideDish: "#C6BCA2",
-        dessert: "#DCBDBD",
-        best: "#818E6F",
-        // 카테고리 텍스트
-        riceText: "#A27500",
-        soupText: "#AB4700",
-        sideDishText: "#513A00",
-        dessertText: "#A06262",
-        bestText: "white",
-        //color
-        Gray10: "#EBEDED",
-        Gray20: "#D1D1D1",
-        Gray30: "#A8A8A8",
-        Gray40: "#656565",
-        Gray50: "#1F1F1F",
-      },
-      screens: {
-        xs: "480px",
-      },
-    },
+  	extend: {
+  		colors: {
+  			inputGray: '#F1F1F1',
+  			materialGet: '#9A9A9A',
+  			recipeFind: '#FFA2A2',
+  			recipeSearch: '#B56D40',
+  			freezerSearch: '#BFB2A9',
+  			rice: '#FDCD4F',
+  			soup: '#FFA86A',
+  			sideDish: '#C6BCA2',
+  			dessert: '#DCBDBD',
+  			best: '#818E6F',
+  			riceText: '#A27500',
+  			soupText: '#AB4700',
+  			sideDishText: '#513A00',
+  			dessertText: '#A06262',
+  			bestText: 'white',
+  			Gray10: '#EBEDED',
+  			Gray20: '#D1D1D1',
+  			Gray30: '#A8A8A8',
+  			Gray40: '#656565',
+  			Gray50: '#1F1F1F',
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		screens: {
+  			xs: '480px'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
